84a74489fb71d80246bcbf67a71c51fa991d8adc
edd34 <eddine.farid@hotmail.fr>
2022-05-11 01:19:38 +0300

    remove gégé label and remove invalid tag p

--- components/AuthorIntro.vue
+++ components/AuthorIntro.vue
@@ -1,25 +1,43 @@
 <template>
   <div class="mb-6">
     <h2
-      class="mt-6 text-4xl text-gray-700 font-bold capitalize dark:text-blue-200"
+      class="
+        mt-6
+        text-4xl text-gray-700
+        font-bold
+        capitalize
+        dark:text-blue-200
+      "
     >
-      👋 Salut, Gégé !
+      👋 Salut !
     </h2>
-    <br/>
+    <br />
 
-      <p class="text-justify">
+    <p class="text-justify">
       Moi c'est Eddine, développeur freelance spécialisé en Python, enchanté.
-      <p/>
-
-      <br/>
-
-      <p class="text-justify">
-      Passionné de l'ingénierie logicielle, l'IT, la tech, l'Open Source, le business... mais aussi curieux, autodidacte, je me pousse souvent hors de ma zone de confort, attiré par une soif de connaissance insatiable. J'adore résoudre des problèmes techniques, j'ai l'amour du travail bien fait dans mon ADN. 
-      <p/>
-      <p class="text-justify">
-      Ma plus grande peur est de n'avoir rien à faire, rien à apprendre c'est pour cela que je complète mon emploi du temps avec des projets perso, ou de la veille sur les secteurs industriels ainsi que les tendances et technologies auxquelles je me projette.
-      <p/>
-      Au plaisir d'échanger. N'hésitez pas à parcours le blog en attendant.
     </p>
+
+    <p />
+
+    <br />
+
+    <p class="text-justify">
+      Passionné de l'ingénierie logicielle, l'IT, la tech, l'Open Source, le
+      business... mais aussi curieux, autodidacte, je me pousse souvent hors de
+      ma zone de confort, attiré par une soif de connaissance insatiable.
+      J'adore résoudre des problèmes techniques, j'ai l'amour du travail bien
+      fait dans mon ADN.
+    </p>
+
+    <p />
+    <p class="text-justify">
+      Ma plus grande peur est de n'avoir rien à faire, rien à apprendre c'est
+      pour cela que je complète mon emploi du temps avec des projets perso, ou
+      de la veille sur les secteurs industriels ainsi que les tendances et
+      technologies auxquelles je me projette.
+    </p>
+
+    <p />
+    Au plaisir d'échanger. N'hésitez pas à parcours le blog en attendant.
   </div>
 </template>
