65d45b56459cb4addae965395fed58958e8220ce
edd34 <eddine.farid@hotmail.fr>
2022-05-11 01:36:19 +0300

    update blog index file

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,12 +2,21 @@
   <div>
     <header class="pt-16 pb-9 sm:pb-16 sm:text-center">
       <h1
-        class="mb-4 text-8xl sm:text-4xl tracking-tight text-slate-800 font-extrabold dark:text-slate-200"
+        class="
+          mb-4
+          text-8xl
+          sm:text-4xl
+          tracking-tight
+          text-slate-800
+          font-extrabold
+          dark:text-slate-200
+        "
       >
         Blog
       </h1>
       <p class="text-lg text-slate-700 dark:text-slate-400">
-        All the latest Tailwind CSS news, straight from the&nbsp;team.
+        Tous les derniers articles en lien avec Python, Git, le développement
+        logiciel...
       </p>
     </header>
     <div class="space-y-16 mx-auto max-w-7xl">
@@ -45,14 +54,14 @@ export default {
     };
   },
   head: {
-    title: "Md Solaiman | Blogs",
+    title: "Eddine Omar | Blogs",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
         hid: "description",
         name: "description",
-        content: "Its Solaiman's Pen and Paper to write ",
+        content: "Bienvenue sur le devblog d'Eddine",
       },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
