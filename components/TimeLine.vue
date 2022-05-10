2b7f34f7816f3a44c40a0525f2f29347db1f8847
edd34 <eddine.farid@hotmail.fr>
2022-05-11 01:18:44 +0300

    update timeline

--- components/TimeLine.vue
+++ components/TimeLine.vue
@@ -33,7 +33,7 @@
             dark:text-white
           "
         >
-          Software Engineer at FANG
+          Formateur @ CCI Mayotte
           <span
             class="
               bg-blue-100
@@ -46,7 +46,7 @@
               dark:bg-blue-200 dark:text-blue-800
               ml-3
             "
-            >Currently</span
+            >Actuellement</span
           >
         </h3>
         <time
@@ -59,11 +59,82 @@
             text-gray-400
             dark:text-gray-500
           "
-          >2018-Currently</time
+          >Janvier 2022 - Aujourd'hui</time
         >
         <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-          Get access to over 20+ pages including a dashboard layout, charts,
-          kanban board, calendar, and pre-order E-commerce & Marketing pages.
+          Cours sur Java orienté objet pour les BTS SIO 1ère année.
+          <br />
+          Titre professionnel REMN de niveau Bac+2 : Responsable d'Espace de
+          Médiation Numérique.
+        </p>
+      </li>
+      <li class="mb-10 ml-6">
+        <span
+          class="
+            flex
+            absolute
+            -left-3
+            justify-center
+            items-center
+            w-6
+            h-6
+            bg-blue-200
+            rounded-full
+            ring-8 ring-white
+            dark:ring-gray-900 dark:bg-blue-900
+          "
+        >
+          <img
+            class="w-3 h-3 text-blue-600 dark:text-blue-400"
+            src="~assets/icon/academy.svg"
+          />
+        </span>
+        <h3
+          class="
+            flex
+            items-center
+            mb-1
+            text-lg
+            font-semibold
+            text-gray-900
+            dark:text-white
+          "
+        >
+          Mentor Python @ OpenClassRooms
+          <span
+            class="
+              bg-blue-100
+              text-blue-800 text-sm
+              font-medium
+              mr-2
+              px-2.5
+              py-0.5
+              rounded
+              dark:bg-blue-200 dark:text-blue-800
+              ml-3
+            "
+            >Aujourd'hui</span
+          >
+        </h3>
+        <time
+          class="
+            block
+            mb-2
+            text-sm
+            font-normal
+            leading-none
+            text-gray-400
+            dark:text-gray-500
+          "
+          >2021 - Aujourd'hui</time
+        >
+        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+          Mentor Openclassroom parcours développement d'applications en python,
+          j'aide les élèves dans leurs parcours d'apprentissage du langage
+          Python à l'aide de séance de mentorat.Mentor Openclassroom parcours
+          développement d'applications en python, j'aide les élèves dans leurs
+          parcours d'apprentissage du langage Python à l'aide de séance de
+          mentorat.
         </p>
       </li>
       <li class="mb-10 ml-6">
@@ -88,7 +159,7 @@
           />
         </span>
         <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-          Freelancer
+          Développeur python freelance
         </h3>
         <time
           class="
@@ -100,12 +171,9 @@
             text-gray-400
             dark:text-gray-500
           "
-          >Released on December 7th, 2021</time
+          >2020 - Aujourd'hui</time
         >
-        <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-          All of the pages and components are first designed in Figma and we
-          keep a parity between the two versions even as we update the project.
-        </p>
+        <p class="text-base font-normal text-gray-500 dark:text-gray-400"></p>
       </li>
       <li class="mb-10 ml-6">
         <span
@@ -129,7 +197,7 @@
           />
         </span>
         <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-          2013 - Dhaka University
+          Ingénieur en informatique industrielle - Polytech Montpellier
         </h3>
         <time
           class="
@@ -141,10 +209,59 @@
             text-gray-400
             dark:text-gray-500
           "
-          >2013 - 2018
+          >2015 - 2018
         </time>
         <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-          Studied "BSC in Computer Science and Engineering" in Dhaka University.
+          Cette formation m’a permis de consolider mes connaissances dans
+          l'informatique industrielle au travers d’enseignements théoriques et
+          pratiques lors des projets scolaires ainsi qu’au travers des stages.
+          <br />
+          TOEIC obtenu avec un score de 845/990.
+        </p>
+      </li>
+      <li class="mb-10 ml-6">
+        <span
+          class="
+            flex
+            absolute
+            -left-3
+            justify-center
+            items-center
+            w-6
+            h-6
+            bg-blue-200
+            rounded-full
+            ring-8 ring-white
+            dark:ring-gray-900 dark:bg-blue-900
+          "
+        >
+          <img
+            class="w-3 h-3 text-blue-600 dark:text-blue-400"
+            src="~assets/icon/academy.svg"
+          />
+        </span>
+        <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+          Prépa Intégré - Polytech Lille
+        </h3>
+        <time
+          class="
+            block
+            mb-2
+            text-sm
+            font-normal
+            leading-none
+            text-gray-400
+            dark:text-gray-500
+          "
+          >2013 - 2015
+        </time>
+        <p class="text-base font-normal text-gray-500 dark:text-gray-400">
+          Parcours Sciences de l'Ingénieur.
+          <br />
+          La prépa intégré de Polytech Lille forme et sensibilise les futurs
+          ingénieurs au futur monde de l’ingénierie, au travers de cours
+          théoriques et pratiques dans divers domaines tels que la mécanique,
+          l’informatique, les mathématiques et la physique.
         </p>
       </li>
       <li class="mb-10 ml-6">
@@ -169,7 +286,7 @@
           />
         </span>
         <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-          Born in 1996, July 27
+          Né en 1995
         </h3>
         <time
           class="
@@ -181,10 +298,11 @@
             text-gray-400
             dark:text-gray-500
           "
-          >Dhaka Bangladesh</time
+          >Mayotte</time
         >
         <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-          I couldn't remember any productive work in that time. But
+          Je ne me souviens pas avoir effectué quoi que ce soit de productif
+          cette année, mais peu importe.
         </p>
       </li>
     </ol>
