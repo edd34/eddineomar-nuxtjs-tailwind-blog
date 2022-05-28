a4f0e5a4f8e8623ef14b0b205f3069856f96c790
edd34 <eddine.farid@hotmail.fr>
2022-05-28 09:17:02 +0300

    enable menu projects

--- pages/projects.vue
+++ pages/projects.vue
@@ -2,12 +2,21 @@
   <div class="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">
     <div class="pt-6 pb-8 space-y-2 md:space-y-5">
       <h1
-        class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14"
+        class="
+          text-3xl
+          font-extrabold
+          leading-9
+          tracking-tight
+          text-gray-900
+          dark:text-gray-100
+          sm:text-4xl sm:leading-10
+          md:text-6xl md:leading-14
+        "
       >
         Projects
       </h1>
       <p class="text-lg leading-7 text-gray-500 dark:text-gray-400">
-        Some of the projects I've build or been a part of.
+        Parmis les projets dont j'ai réalisés !
       </p>
     </div>
     <main class="relative mb-auto">
@@ -41,14 +50,14 @@ export default {
     };
   },
   head: {
-    title: "Md Solaiman | Projects",
+    title: "Eddine OMAR | Projets",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
         hid: "description",
         name: "description",
-        content: "Some of Projects developed by Solaiman",
+        content: "Quelques projets réalisés par Eddine OMAR",
       },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
