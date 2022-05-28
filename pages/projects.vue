557f66d03063de50648790530ec888fde31b11e8
edd34 <eddine.farid@hotmail.fr>
2022-05-28 11:11:37 +0300

    move hint

--- pages/projects.vue
+++ pages/projects.vue
@@ -21,10 +21,6 @@
         également des projets cryptos avec web3py, solidity et plein d'autres à
         venir !
       </p>
-      <p class="text-m leading-7 text-gray-500 dark:text-gray-400 italic">
-        PSSST!? Au fait, ça vous dirait d'aller mettre des 🌟 dans mes projets
-        perso sur Github ?
-      </p>
     </div>
     <main class="relative mb-auto">
       <div class="">
@@ -46,6 +42,10 @@
         </div>
       </div>
     </main>
+    <p class="text-m leading-7 text-gray-500 dark:text-gray-400 italic">
+      PSSST!? Au fait, ça vous dirait d'aller mettre des 🌟 dans mes projets
+      perso sur Github ?
+    </p>
   </div>
 </template>
 
