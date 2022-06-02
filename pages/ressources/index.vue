6923dea797530eaedd37792744e877ec1fe68d3e
edd34 <eddine.farid@hotmail.fr>
2022-06-02 21:17:41 +0300

    update favicon

--- pages/ressources/index.vue
+++ pages/ressources/index.vue
@@ -65,7 +65,13 @@ export default {
         content: "Bienvenue sur le devblog d'Eddine",
       },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💻</text></svg>",
+      },
+    ],
   },
 };
 </script>
