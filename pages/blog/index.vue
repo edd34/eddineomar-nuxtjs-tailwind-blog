9a44e460f225c1360d38d4c512748f6bd3d5bd93
edd34 <eddine.farid@hotmail.fr>
2022-11-04 11:10:58 +0100

    update title

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -55,7 +55,7 @@ export default {
     };
   },
   head: {
-    title: "🚀Eddine Omar - Lead dev Python / DevOps | Blogs 📰",
+    title: "🚀Eddine Omar | Blogs 📰",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
--- pages/ressources/index.vue
+++ pages/ressources/index.vue
@@ -55,7 +55,7 @@ export default {
     };
   },
   head: {
-    title: "🚀Eddine Omar - Lead dev Python / DevOps | Ressources 📰",
+    title: "🚀Eddine Omar | Ressources 📰",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
